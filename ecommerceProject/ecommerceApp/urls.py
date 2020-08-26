e6863b7225fc8b8f6fe86ed770f07b492695869f
Leslie Zhou <lesliezhou2012@gmail.com>
2020-08-25 22:30:45 -0700

    adding some html

--- ecommerceProject/ecommerceApp/urls.py
+++ ecommerceProject/ecommerceApp/urls.py
@@ -1,8 +1,8 @@
-
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index),
-
+    path('men', views.men),
+    path('women', views.women),
 ]
--- ecommerceProject/ecommerceApp/views.py
+++ ecommerceProject/ecommerceApp/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import redirect, render
 def index(request):
     return render(request, 'index.html')
 
+def men(request):
+    return render(request, 'men.html')
+
+def women(request):
+    return render(request, 'women.html')
